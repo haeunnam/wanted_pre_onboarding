7477267b657127bd1bc75ab76eb12807c20b6ac4
grace <plain3785@gmail.com>
2022-02-06 00:00:01 +0900

    type 검사 수정

--- custom-component/src/component/ClickToEdit.js
+++ custom-component/src/component/ClickToEdit.js
@@ -8,14 +8,16 @@ function ClickToEdit() {
   function onBlurName(e) {
     if (!isNaN(e.target.value)) {
       alert("문자만 입력가능합니다.");
+      e.target.value = "";
       return;
     }
     setName(e.target.value);
   }
 
   function onBlurAge(e) {
-    if (isNaN(e.target.value)) {
-      alert("숫자만 입력가능합니다.");
+    if (isNaN(e.target.value) || !Number.isInteger(Number(e.target.value))) {
+      alert("정확한 값을 입력해주세요.");
+      e.target.value = "";
       return;
     }
     setAge(e.target.value);
